efb1a855ff4b457d52c44c810146fb962776075c
ainiroad <mikeainiroad@gmail.com>
2025-09-07 19:40:18 +0800

    add begin end
    Signed-off-by: ainiroad <mikeainiroad@gmail.com>

--- rest/api/paging.go
+++ rest/api/paging.go
@@ -3,5 +3,7 @@ package api
 type PagingParam struct {
 	After  int64 `url:"after,omitempty"`
 	Before int64 `url:"before,omitempty"`
+	Begin  int64 `url:"begin,omitempty"`
+	End    int64 `url:"end,omitempty"`
 	Limit  int   `url:"limit,omitempty"`
 }
